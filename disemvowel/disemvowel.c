d025357f8a49f4e7d1c58bf94aa16c91f895e033
Avery Koranda <koran023@morris.umn.edu>
2018-09-25 11:09:49 -0500

    First draft of code. No errors, some tests pass

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,15 +2,9 @@
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-	
+bool isVowel(char letter) {
 
-
-  return (char*) "";
-}
-
-bool isVowel(char *letter) {
-  char *vowels = calloc(11, sizeof(char));
+  char *vowels = (char*) calloc(11, sizeof(char));
   vowels[0] = 'a';
   vowels[1] = 'e';
   vowels[2] = 'i';
@@ -24,13 +18,53 @@ bool isVowel(char *letter) {
   vowels[10] = '\0';
 
   // referenced: https://stackoverflow.com/questions/15094834/check-if-a-value-exist-in-a-array
+  bool isContained = false;
   int i;
   for (i=0; i < 10; i++) {
  	 if(vowels[i] == letter){
- 	  free(vowels);
-	  return true;
-  }
+	  isContained = true;
+  	 }
   }
   free(vowels);
-  return false;
+  return isContained;
 }
+
+int numNonVowels(char *word) {
+	//https://www.quora.com/How-do-I-find-the-length-of-an-array-in-C
+	int length = sizeof(word)/sizeof(char);
+	//start at 1 because of null terminator
+	int result = 1;
+	int i;
+	for (i=0; i < length; i++) {
+		if(!isVowel(word[i])){
+			result++;
+		}
+	}
+	return result;
+}
+
+void  removeVowels(char *str, char *result) {
+	int length = sizeof(str)/sizeof(char);
+	int i;
+	int j = 0;
+	for (i = 0; i < length; i++) {
+		if (!isVowel(str[i])){
+			result[j] = str[i];
+			j++;
+		}
+	}
+       result[j +1] = '\0';
+}
+
+char *disemvowel(char *str) {
+
+  // find size of resulting array
+  int size = numNonVowels(str);
+
+  char *result = (char*) calloc(size, sizeof(char));
+
+  removeVowels(str, result);
+
+  return result;
+}
+
