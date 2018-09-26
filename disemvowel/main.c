23771e5cf36cfd735b8b95d270176cc7db676596
Leah Judd <juddx097@acrylic.morris.umn.edu>
2018-09-26 15:41:00 -0500

    Fixed leaks in main.c for disemvowel

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -9,8 +9,11 @@ int main(int argc, char *argv[]) {
   
   size = 100;
   line = (char*) malloc (size + 1);
-
+  
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+	 char *result = disemvowel(line);
+    printf("%s\n", result);
+    free(result);
   }
+  free(line);
 }
